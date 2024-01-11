e5172be17d856e55c04732da3232bb07e16c339f
immanueljanis <immanuelj4nis@gmail.com>
2024-01-12 02:04:52 +0700

    build: feat_crudAdmin

--- apps/api/src/controllers/AdminController.ts
+++ apps/api/src/controllers/AdminController.ts
@@ -2,17 +2,25 @@ import { Request, Response } from "express"
 import prisma from "@/prisma"
 
 export const getAllAdmin = async (req: Request, res: Response) => {
-    const adminData = await prisma.user.findMany({
-        where: {
-            role: "admin"
-        }
-    })
+    try {
+        const adminData = await prisma.user.findMany({
+            where: {
+                role: "admin"
+            }
+        })
 
-    return res.status(200).send(adminData)
-}
-
-export const testApi = (req: Request, res: Response) => {
-    res.send("test api")
+        res.status(200).send({
+            error: false,
+            message: "Get Data Success",
+            data: adminData
+        })
+    } catch (error: any) {
+        res.status(500).send({
+            error: true,
+            message: error.message,
+            data: null
+        })
+    }
 }
 
 export const createAdmin = async (req: Request, res: Response) => {
@@ -37,8 +45,100 @@ export const createAdmin = async (req: Request, res: Response) => {
             message: `Create data admin ${name} success`,
             data: null
         })
-    } catch (error) {
-        console.log(error)
+    } catch (error: any) {
+        res.status(500).send({
+            error: true,
+            message: error.message,
+            data: null
+        })
     }
 
+}
+
+export const getAdminById = async (req: Request, res: Response) => {
+    try {
+        const data = await prisma.user.findUnique({
+            where: {
+                id: String(req?.query?.id)
+            }
+        })
+
+        if (!data) throw { message: "data not found" }
+
+        res.status(200).send({
+            error: false,
+            message: "Data found",
+            data: data
+        })
+    } catch (error: any) {
+        res.status(500).send({
+            error: true,
+            message: error.message,
+            data: null
+        })
+    }
+}
+
+export const updateAdminById = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.query
+        const { email, name, password, phone_number, address, image } = req.body
+
+        console.log(req.params)
+        const existingAdmin = await prisma.user.findUnique({
+            where: {
+                id: String(id)
+            }
+        })
+
+        if (!existingAdmin) { res.status(404).send({ message: 'Admin not found' }) }
+
+        const updatedAdmin = await prisma.user.update({
+            where: {
+                id: String(id)
+            },
+            data: {
+                email: email || existingAdmin?.email,
+                name: name || existingAdmin?.name,
+                password: password || existingAdmin?.password,
+                phone_number: phone_number || existingAdmin?.phone_number,
+                address: address || existingAdmin?.address,
+                image: image || existingAdmin?.image,
+            }
+        })
+
+        return res.status(200).send({
+            error: false,
+            message: "Edit admin success",
+            data: updatedAdmin
+        })
+    } catch (error: any) {
+        res.status(500).send({
+            error: true,
+            message: error.message,
+            data: null
+        })
+    }
+}
+
+export const deleteAdminById = async (req: Request, res: Response) => {
+    try {
+        await prisma.user.delete({
+            where: {
+                id: String(req?.query?.id)
+            }
+        })
+
+        res.status(200).send({
+            error: false,
+            message: "Data delete success",
+            data: null
+        })
+    } catch (error: any) {
+        res.status(500).send({
+            error: true,
+            message: error.message,
+            data: null
+        })
+    }
 }
