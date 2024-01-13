f0e53c75092b5725234447afc6b8d27bb2b42101
immanueljanis <immanuelj4nis@gmail.com>
2024-01-13 12:50:35 +0700

    feat: crudCategory

--- apps/api/src/routers/Index.ts
+++ apps/api/src/routers/Index.ts
@@ -5,8 +5,11 @@ router.use(express.json())
 
 import AdminRouter from "./AdminRouter"
 import UserRouter from "./UserRouter"
+import CategoryRouter from "./CategoryRouter"
 
 router.use("/api/admin", AdminRouter)
 router.use("/api/user", UserRouter)
+router.use("/api/category", CategoryRouter)
+
 
 export default router
