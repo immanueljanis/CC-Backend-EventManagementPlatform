b2ebc4c7408010613cb3ebbde92c4deeb8019dfb
immanueljanis <immanuelj4nis@gmail.com>
2024-01-25 16:09:47 +0700

    fix: setup dashboard

--- apps/web/src/app/admin/event/page.js
+++ apps/web/src/app/admin/event/page.js
@@ -1,14 +1,14 @@
 import { PlusSquare } from 'lucide-react'
-import { Tables} from './../../../components/table'
+import { Tables } from './../../../components/table'
 
-export default function Page(){
-    return(
+export default function Page() {
+    return (
         <div className='w-full py-4'>
-               
-                <div className="w-full px-5">
-                    <Tables/>
-                </div>
-            
+
+            <div className="w-full px-5">
+                <Tables />
+            </div>
+
         </div>
     )
 }
--- apps/web/src/components/table.js
+++ apps/web/src/components/table.js
@@ -1,18 +1,18 @@
 'use client'
 import axios from 'axios';
-import { Delete, Pencil, Check , X } from 'lucide-react';
+import { Delete, Pencil, Check, X } from 'lucide-react';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { FcViewDetails } from "react-icons/fc";
 import { useParams } from 'next/navigation';
 
 
-export default function Table(){
-    
+export default function Table() {
+
     const [data, setData] = useState([])
     const params = useParams()
 
-    const fetchData = async() => {
+    const fetchData = async () => {
         try {
             const res = await fetch(`http://localhost:8001/events/`)
             const tableData = await res.json()
@@ -21,53 +21,53 @@ export default function Table(){
             console.log(error)
         }
     }
-useEffect(() => {
-    fetchData()
-})
+    useEffect(() => {
+        fetchData()
+    })
 
-    return(
+    return (
         <div className="overflow-x-auto ">
             <table className="table table-xs">
                 <thead>
-                <tr>
-                    <th></th> 
-                    <th>Event Name</th> 
-                    <th>Organizer Name</th> 
-                    <th>Location</th> 
-                    <th>Date</th> 
-                    <th>Status</th> 
-                    <th>Update</th>
-                    <th>Delete</th>
-                </tr>
-                </thead> 
+                    <tr>
+                        <th></th>
+                        <th>Event Name</th>
+                        <th>Organizer Name</th>
+                        <th>Location</th>
+                        <th>Date</th>
+                        <th>Status</th>
+                        <th>Update</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
                 <tbody>
-                
-                {data.map((item,index)=>{
-                    return(
-                        <tr>
-                        <th>{index+1}</th> 
-                        <td>{item.tittle}</td> 
-                        <td>{item.organizerName}</td> 
-                        <td>{item.location}</td> 
-                        <td>{item.date}</td> 
-                        <td>{item.status}</td> 
-                        <td><Link href={`/organizer/event/${item.id}`}><button className='text-blue-500'><Pencil/></button></Link></td>
-                        <td><button className='text-red-500'><Delete/></button></td>
-                        </tr>
-                
-                    )
-                })}
-                
-                </tbody> 
+
+                    {data.map((item, index) => {
+                        return (
+                            <tr>
+                                <th>{index + 1}</th>
+                                <td>{item.tittle}</td>
+                                <td>{item.organizerName}</td>
+                                <td>{item.location}</td>
+                                <td>{item.date}</td>
+                                <td>{item.status}</td>
+                                <td><Link href={`/organizer/event/${item.id}`}><button className='text-blue-500'><Pencil /></button></Link></td>
+                                <td><button className='text-red-500'><Delete /></button></td>
+                            </tr>
+
+                        )
+                    })}
+
+                </tbody>
             </table>
         </div>
     )
 }
 
-export function Tables(){
+export function Tables() {
     const [data, setData] = useState([])
 
-    const fetchData = async() => {
+    const fetchData = async () => {
         try {
             const res = await fetch('http://localhost:8001/events')
             const tableData = await res.json()
@@ -78,44 +78,44 @@ export function Tables(){
     }
     useEffect(() => {
         fetchData()
-    },[])
-    return(
+    }, [])
+    return (
         (
             <div className="overflow-x-auto ">
                 <table className="table table-xs">
                     <thead>
-                    <tr>
-                        <th></th> 
-                        <th>Event Name</th> 
-                        <th>Organizer Name</th> 
-                        <th>Location</th> 
-                        <th>Date</th> 
-                        <th>Status</th> 
-                        <th>Detail</th> 
-                        <th>Accept</th>
-                        <th>Decline</th>
-                    </tr>
-                    </thead> 
+                        <tr>
+                            <th></th>
+                            <th>Event Name</th>
+                            <th>Organizer Name</th>
+                            <th>Location</th>
+                            <th>Date</th>
+                            <th>Status</th>
+                            <th>Detail</th>
+                            <th>Accept</th>
+                            <th>Decline</th>
+                        </tr>
+                    </thead>
                     <tbody>
-                    
-                    {data.map((item,index)=>{
-                        return(
-                            <tr>
-                            <th>{index+1}</th> 
-                            <td>{item.tittle}</td> 
-                            <td>{item.organizerName}</td> 
-                            <td>{item.location}</td> 
-                            <td>{item.date}</td> 
-                            <td>{item.status}</td> 
-                            <td><button><Link href={`/admin/event/${item.id}`}><FcViewDetails className='text-xl'/></Link></button></td> 
-                            <td><button className='text-blue-500'><Check/></button></td>
-                            <td><button className='text-red-500'><X/></button></td>
-                            </tr>
-                    
-                        )
-                    })}
-                    
-                    </tbody> 
+
+                        {data.map((item, index) => {
+                            return (
+                                <tr>
+                                    <th>{index + 1}</th>
+                                    <td>{item.tittle}</td>
+                                    <td>{item.organizerName}</td>
+                                    <td>{item.location}</td>
+                                    <td>{item.date}</td>
+                                    <td>{item.status}</td>
+                                    <td><button><Link href={`/admin/event/${item.id}`}><FcViewDetails className='text-xl' /></Link></button></td>
+                                    <td><button className='text-blue-500'><Check /></button></td>
+                                    <td><button className='text-red-500'><X /></button></td>
+                                </tr>
+
+                            )
+                        })}
+
+                    </tbody>
                 </table>
             </div>
         )
